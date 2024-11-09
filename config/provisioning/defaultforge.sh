1678623d6f2cfd2b00d8d4e2664e2bfa56e2fa43
dfksmasher <pete@komodohardware.co.uk>
2024-11-09 19:49:01 +0000

    Update defaultforge.sh

--- config/provisioning/defaultforge.sh
+++ config/provisioning/defaultforge.sh
@@ -2,18 +2,50 @@
 # This file will be sourced in init.sh
 # Namespace functions with provisioning_
 
-# https://raw.githubusercontent.com/ai-dock/stable-diffusion-webui/main/config/provisioning/default.sh
+# Provisioning script for stable-diffusion-webui-reForge (dev_upstream branch)
 
 ### Edit the following arrays to suit your workflow - values must be quoted and separated by newlines or spaces.
-### If you specify gated models you'll need to set environment variables HF_TOKEN and/or CIVITAI_TOKEN
-EXTENSIONS=(
-   "https://github.com/DominikDoom/a1111-sd-webui-tagcomplete"
-   "https://github.com/ArtVentureX/sd-webui-agent-scheduler"
-   "https://github.com/dfksmasher/sd-webui-ar"
-   "https://github.com/dfksmasher/sd-dynamic-prompts"
-   "https://github.com/AlUlkesh/stable-diffusion-webui-images-browser"  
+### If you specify gated models, you'll need to set environment variables HF_TOKEN and/or CIVITAI_TOKEN
+
+DISK_GB_REQUIRED=30
+
+APT_PACKAGES=(
+    # Add any additional APT packages if needed
 )
-# [Your arrays: DISK_GB_REQUIRED, APT_PACKAGES, PIP_PACKAGES, EXTENSIONS, CHECKPOINT_MODELS, etc.]
+
+PIP_PACKAGES=(
+    # Add any additional PIP packages if needed
+)
+
+EXTENSIONS=(
+    "https://github.com/DominikDoom/a1111-sd-webui-tagcomplete"
+    "https://github.com/ArtVentureX/sd-webui-agent-scheduler"
+    "https://github.com/dfksmasher/sd-webui-ar"
+    "https://github.com/dfksmasher/sd-dynamic-prompts"
+    "https://github.com/AlUlkesh/stable-diffusion-webui-images-browser"
+)
+
+CHECKPOINT_MODELS=(
+    "https://civitai.com/api/download/models/1022833"
+    "https://civitai.com/api/download/models/1023901"
+    "https://civitai.com/api/download/models/962003"
+)
+
+LORA_MODELS=(
+    # Add any LoRA models if needed
+)
+
+VAE_MODELS=(
+    # Add any VAE models if needed
+)
+
+ESRGAN_MODELS=(
+    # Add any ESRGAN models if needed
+)
+
+CONTROLNET_MODELS=(
+    # Add any ControlNet models if needed
+)
 
 ### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
 
@@ -24,7 +56,7 @@ function provisioning_start() {
     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
     sudo update-alternatives --set python3 /usr/bin/python3.10
 
-    # We need to apply some workarounds to make old builds work with the new default
+    # Apply workarounds if needed
     if [[ ! -d /opt/environments/python ]]; then 
         export MAMBA_BASE=true
     fi
@@ -34,8 +66,13 @@ function provisioning_start() {
     # Remove existing repository if it exists
     rm -rf /opt/stable-diffusion-webui
 
-    # Clone the reForge repository
-    git clone https://github.com/Panchovix/stable-diffusion-webui-reForge.git /opt/stable-diffusion-webui
+    # Clone the reForge repository (dev_upstream branch)
+    git clone --branch dev_upstream https://github.com/Panchovix/stable-diffusion-webui-reForge.git /opt/stable-diffusion-webui
+
+    # Alternatively, you can use:
+    # git clone https://github.com/Panchovix/stable-diffusion-webui-reForge.git /opt/stable-diffusion-webui
+    # cd /opt/stable-diffusion-webui
+    # git checkout dev_upstream
 
     DISK_GB_AVAILABLE=$(($(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000))
     DISK_GB_USED=$(($(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000))
@@ -56,7 +93,7 @@ function provisioning_start() {
         "/opt/stable-diffusion-webui/models/Stable-diffusion" \
         "${CHECKPOINT_MODELS[@]}"
     provisioning_get_models \
-        "/opt/stable-diffusion-webui/models/lora" \
+        "/opt/stable-diffusion-webui/models/Lora" \
         "${LORA_MODELS[@]}"
     provisioning_get_models \
         "/opt/stable-diffusion-webui/models/ControlNet" \
@@ -73,7 +110,7 @@ function provisioning_start() {
         PLATFORM_ARGS="--use-cpu all --skip-torch-cuda-test --no-half"
     fi
     PROVISIONING_ARGS="--skip-python-version-check --no-download-sd-model --do-not-download-clip --port 11404 --exit"
-    ARGS_COMBINED="${PLATFORM_ARGS} $(cat /etc/a1111_webui_flags.conf) ${PROVISIONING_ARGS}"
+    ARGS_COMBINED="${PLATFORM_ARGS} $(cat /etc/a1111_webui_flags.conf 2>/dev/null) ${PROVISIONING_ARGS}"
 
     # Start and exit because webui will probably require a restart
     cd /opt/stable-diffusion-webui
@@ -97,7 +134,85 @@ function pip_install() {
     fi
 }
 
-# Add the missing functions here
-# [Include provisioning_get_apt_packages, provisioning_get_extensions, provisioning_get_models, provisioning_download, and any other necessary functions]
+function provisioning_get_apt_packages() {
+    if [[ -n "${APT_PACKAGES[*]}" ]]; then
+        sudo apt-get install -y "${APT_PACKAGES[@]}"
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n "${PIP_PACKAGES[*]}" ]]; then
+        pip_install "${PIP_PACKAGES[@]}"
+    fi
+}
+
+function provisioning_get_extensions() {
+    for repo in "${EXTENSIONS[@]}"; do
+        dir="${repo##*/}"
+        path="/opt/stable-diffusion-webui/extensions/${dir}"
+        if [[ -d $path ]]; then
+            # Pull only if AUTO_UPDATE
+            if [[ ${AUTO_UPDATE,,} == "true" ]]; then
+                printf "Updating extension: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+            fi
+        else
+            printf "Downloading extension: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+        fi
+    done
+}
+
+function provisioning_get_models() {
+    if [[ -z $2 ]]; then return 1; fi
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    if [[ $DISK_GB_ALLOCATED -ge $DISK_GB_REQUIRED ]]; then
+        arr=("$@")
+    else
+        printf "WARNING: Low disk space allocation - Only the first model will be downloaded!\n"
+        arr=("$1")
+    fi
+
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
+
+function provisioning_download() {
+    if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
+        auth_token="$HF_TOKEN"
+    elif [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
+        auth_token="$CIVITAI_TOKEN"
+    fi
+    if [[ -n $auth_token ]]; then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    else
+        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    fi
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n"
+    printf "#                                            #\n"
+    printf "#          Provisioning container            #\n"
+    printf "#                                            #\n"
+    printf "#         This will take some time           #\n"
+    printf "#                                            #\n"
+    printf "# Your container will be ready on completion #\n"
+    printf "#                                            #\n"
+    printf "##############################################\n\n"
+    if [[ $DISK_GB_ALLOCATED -lt $DISK_GB_REQUIRED ]]; then
+        printf "WARNING: Your allocated disk size (%sGB) is below the recommended %sGB - Some models will not be downloaded\n" "$DISK_GB_ALLOCATED" "$DISK_GB_REQUIRED"
+    fi
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete: Web UI will start now\n\n"
+}
 
 provisioning_start
