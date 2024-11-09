93109d27373fc52cec3ee893adb67ad7aa5af612
dfksmasher <pete@komodohardware.co.uk>
2024-11-09 19:19:19 +0000

    Create defaultforge.sh

---
+++ config/provisioning/defaultforge.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# This file will be sourced in init.sh
+# Namespace functions with provisioning_
+
+# ... [Keep the initial comments and variables as in your original script] ...
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    # Ensure Python 3.10 is installed and set as default
+    sudo apt-get update
+    sudo apt-get install -y python3.10 python3.10-venv python3.10-dev
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
+    sudo update-alternatives --config python3 <<< '2'
+
+    # We need to apply some workarounds to make old builds work with the new default
+    if [[ ! -d /opt/environments/python ]]; then 
+        export MAMBA_BASE=true
+    fi
+    source /opt/ai-dock/etc/environment.sh
+    source /opt/ai-dock/bin/venv-set.sh webui
+
+    # Remove existing repository if it exists
+    rm -rf /opt/stable-diffusion-webui
+
+    # Clone the reForge repository
+    git clone https://github.com/Panchovix/stable-diffusion-webui-reForge.git /opt/stable-diffusion-webui
+
+    DISK_GB_AVAILABLE=$(($(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_USED=$(($(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_ALLOCATED=$(($DISK_GB_AVAILABLE + $DISK_GB_USED))
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_get_pip_packages
+
+    # Install requirements for reForge
+    cd /opt/stable-diffusion-webui
+    pip_install -r requirements.txt
+    if [[ -f requirements_versions.txt ]]; then
+        pip_install -r requirements_versions.txt
+    fi
+
+    provisioning_get_extensions
+    provisioning_get_models \
+        "${WORKSPACE}/stable-diffusion-webui/models/Stable-diffusion" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/stable-diffusion-webui/models/lora" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/stable-diffusion-webui/models/ControlNet" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/stable-diffusion-webui/models/VAE" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/stable-diffusion-webui/models/esrgan" \
+        "${ESRGAN_MODELS[@]}"
+     
+    PLATFORM_ARGS=""
+    if [[ $XPU_TARGET = "CPU" ]]; then
+        PLATFORM_ARGS="--use-cpu all --skip-torch-cuda-test --no-half"
+    fi
+    PROVISIONING_ARGS="--skip-python-version-check --no-download-sd-model --do-not-download-clip --port 11404 --exit"
+    ARGS_COMBINED="${PLATFORM_ARGS} $(cat /etc/a1111_webui_flags.conf) ${PROVISIONING_ARGS}"
+
+    # Start and exit because webui will probably require a restart
+    cd /opt/stable-diffusion-webui
+    if [[ -z $MAMBA_BASE ]]; then
+        source "$WEBUI_VENV/bin/activate"
+        LD_PRELOAD=libtcmalloc.so python webui.py \
+            ${ARGS_COMBINED}
+        deactivate
+    else 
+        micromamba run -n webui -e LD_PRELOAD=libtcmalloc.so python webui.py \
+            ${ARGS_COMBINED}
+    fi
+    provisioning_print_end
+}
+
+function pip_install() {
+    if [[ -z $MAMBA_BASE ]]; then
+        python3.10 -m pip install --no-cache-dir "$@"
+    else
+        micromamba run -n webui pip install --no-cache-dir "$@"
+    fi
+}
+
+# ... [Keep the rest of your functions as in your original script] ...
+
+provisioning_start
